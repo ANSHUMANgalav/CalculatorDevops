eb1c47d71a4bca55a7d24078c9738af4bb3b65ae
[ANSHUMANgalav] <[galavanshuman@gmail.com]>
2021-03-12 23:57:20 +0530

    changes in pom.xml

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,7 +1,10 @@
 package calculator;
 import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class App
-{
+{ private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main(String args[])
     {
         int choice, flag=0;
@@ -41,10 +44,12 @@ public class App
     }
     
     static double Add(double a, double b) {
-    	return a + b;
+    	logger.info("adding numbers "+a +"and"+b);
+        return a + b;
     }
     static double Subtract(double a, double b) {
-    	return a - b;
+        logger.info("subtracting numbers "+a +"and"+b);
+        return a - b;
     }
     static double Multiply(double a, double b) {
     	return a * b;
